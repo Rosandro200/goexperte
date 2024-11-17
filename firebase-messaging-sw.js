611b6a8fffe9add272489e3c1bb4b062776121df
Rosandro200 <75484665+Rosandro200@users.noreply.github.com>
2024-11-17 15:48:54 +0100

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.3.1/firebase-messaging.js');
 
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyBZrLQF4shYXYx_j0qPkqc5f8mRC_Ys1zw",
     authDomain: "expert-66274.firebaseapp.com",
     projectId: "expert-66274",
     storageBucket: "expert-66274.appspot.com",
     messagingSenderId: "728415410069",
     appId: "1:728415410069:web:f94e23eb080132b87f089c"
-});
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
