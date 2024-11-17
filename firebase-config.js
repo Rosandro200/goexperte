8e861151ae2cbca18db258fec2bbc94150ecc202
Rosandro200 <75484665+Rosandro200@users.noreply.github.com>
2024-11-17 09:58:46 +0100

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBZrLQF4shYXYx_j0qPkqc5f8mRC_Ys1zw",
+  apiKey: "AIzaSyCbIQ7u6wlGlmSeB3fnfrjBhG97I6UcJhQ",
   authDomain: "expert-66274.firebaseapp.com",
   databaseURL: "https://expert-66274-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "expert-66274",
-  storageBucket: "expert-66274.appspot.com",
+  storageBucket: "expert-66274.firebasestorage.app",
   messagingSenderId: "728415410069",
-  appId: "1:728415410069:web:f94e23eb080132b87f089c"
+  appId: "1:728415410069:web:f94e23eb080132b87f089c",
+  measurementId: "G-L96GJ1SEK8"
 };
 
 firebase.initializeApp(firebaseConfig);
